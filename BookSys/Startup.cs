c766731110cd480c796cc4f6f233f100c5dfa204
johnreybantoto <johnreybantoto@gmail.com>
2019-07-12 17:33:52 +0800

    started jwt authentication

--- BookSys/Startup.cs
+++ BookSys/Startup.cs
@@ -9,6 +9,10 @@ using BookSys.DAL.Models;
 using Microsoft.EntityFrameworkCore;
 using BookSys.BLL.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System;
 
 namespace BookSys
 {
@@ -38,7 +42,7 @@ namespace BookSys
             services.AddDefaultIdentity<User>()
                 .AddEntityFrameworkStores<BookSysContext>();
 
-            // Overrides custome Identity password rules
+            // Overrides custom Identity password rules
             services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequireDigit = false;
@@ -46,8 +50,28 @@ namespace BookSys
                 options.Password.RequireLowercase = false;
                 options.Password.RequireUppercase = false;
                 options.Password.RequiredLength = 5;
-            }
-          );
+            });
+
+            // for JWT authentication
+            var key = Encoding.UTF8.GetBytes(Configuration["ApplicationSettings:JWT_Secret"].ToString());
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = false;
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
             // add Services in BookSys.BLL
             services.AddScoped<BookService>();
